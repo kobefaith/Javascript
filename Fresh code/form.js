3380275a967e7dd96e3e3c8147558252615e3163
fei zhou <kobefaith@163.com>
2015-12-11 23:20:49 +0800

    add the form.js

---
+++ Fresh code/form.js
@@ -0,0 +1,14 @@
+/**
+ * Created by zhoufei on 2015/12/11.
+ */
+<input type="text" id="email">
+        var  input = document.getElementById('email');
+input.value;
+<label><input type="radio" name="weekday" id="monday" value="1">Moday</label>
+<label><input type="radio" name="weekday" id="thurday" value="2">Thurday</label>
+var mon = document.getElementById('monday');
+var tue = document.getElementById('tuesday');
+mon.value;
+tue.value;
+mon.checked;
+tue.checked;
