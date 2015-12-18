9f10f295681336c41704e6dca316eec51beaefce
fei zhou <kobefaith@163.com>
2015-12-18 12:06:38 +0800

    add the ajax.js

--- Fresh code/ajax.js
+++ Fresh code/ajax.js
@@ -42,5 +42,24 @@ function testCreateXhr(){
         keyword:'c',
         other:'test'
     }));
+    xhr.onreadystatechange = function(e) {
+        if (xhr.readyState === 4 && xhr.status == 200) {
+
+            console.log(xhr.responseText);
+
+            console.log(xhr.responseXML);
+            console.log(xhr.getAllResponseHeaders());
+            console.log(xhr.getResponseHeader("Content-Type"));
+            console.log(xhr.status);
+            console.log(xhr.statusText);
+        } // readyState 5种状态
+        // 0: 未初始化
+        // 1： 连接建立、请求发出
+        // 2: 服务器返回响应
+        // 3: 交互（处理响应数据）
+        // 4: 完成，数据可交付客户端使用
+        //						console.log('onreadystatechange: readyState:%d, status:%d, responseText:%s', xhr.readyState, xhr.status, xhr.responseText);
+        // demo
+    }
 
 }
