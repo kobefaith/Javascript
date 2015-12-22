0b8f016a9b9a977b073ffd9361906c3d82a3888a
fei zhou <kobefaith@163.com>
2015-12-22 22:34:51 +0800

    add the promise.js

--- Fresh code/promise.js
+++ Fresh code/promise.js
@@ -29,3 +29,29 @@ new Promise(test).then(function (result){
 }).catch(function (reason){
     console.log('Ê§°Ü£º' + reason);
 });
+
+
+function ajax(method,url,data){
+    var request = new XMLHttpRequest();
+    return new Promise(function(resolve,reject){
+        request.onreadystatechange = function(){
+            if(request.readyState === 4){
+                if (request.readystate === 200){
+                    resolve(request.responseText);
+                }else{
+                    reject(request.status);
+                }
+            }
+        };
+        request.open(method,url);
+        request.send(data);
+    });
+}
+var log = document.getElementById('test-promise-ajax-result');
+var p = ajax('GET','/api/categories');
+p.then(function (text){
+    log.innerText = text;
+}).catch(function (status){
+    log.innerText = 'ERROR:' + status;
+});
+
