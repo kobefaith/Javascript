8fce4d51ae5df62eba6ff2da559f2df08f27a5f4
fei zhou <kobefaith@163.com>
2015-12-22 22:48:19 +0800

    add the promise.js

--- Fresh code/promise.js
+++ Fresh code/promise.js
@@ -55,3 +55,14 @@ p.then(function (text){
     log.innerText = 'ERROR:' + status;
 });
 
+
+var p1 = new Promise(function (resolve,reject){
+    setTimeout(resolve,500,'P1');
+});
+var p2 = new Promise(function(resolve,reject){
+    setTimeout(resolve,600,'P2');
+});
+Promise.all([p1,p2]).then(function(results){
+    console.log(results);
+});
+
