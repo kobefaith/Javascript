661b62962f7b90fa700c0fcb7bbccc5f47f13230
fei zhou <kobefaith@163.com>
2016-01-10 11:27:28 +0800

    add the underscore.js

--- Fresh code/underscore.js
+++ Fresh code/underscore.js
@@ -26,4 +26,22 @@ var r2 = _.some(obj,function (value,key){
     return key === key.toLowerCase();
 });
 
+var arr = [3,5,7,9];
+_.max(arr);
+_.min(arr);
+_.max({a:1,b:2,c:3});
 
+var scores = [20,81,75,40,91,59,77,66,72,88,99];
+var groups = _.groupBy(scores,function (x){
+    if (x < 60){
+        return 'C';
+    }else if(x < 80){
+        return 'B';
+    } else{
+        return 'A';
+    }
+});
+
+_.shuffle([1,2,3,4,5,6]);
+_.sample([1,2,3,4,5,6]);
+_.sample([1,2,3,4,5,6],3);
