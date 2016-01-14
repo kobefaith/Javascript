771123272a65d23246a582ad74925dd369933b03
fei zhou <kobefaith@163.com>
2016-01-14 21:56:02 +0800

    add the node.js

--- Fresh code/node.js
+++ Fresh code/node.js
@@ -3,3 +3,14 @@
  */
 'use strict';
 console.log('Hello,world');
+
+
+var s = 'Hello';
+function greet(name){
+    console.log(s + ','+name + '!');
+}
+module.experts = greet;
+var greet = require('./hello');
+var s = 'Michael';
+
+greet(s);
