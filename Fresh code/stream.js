37017c977ef3abe05fa4e5110dc40de5610fcb5b
fei zhou <kobefaith@163.com>
2016-01-18 23:25:37 +0800

    add the stream.js

--- Fresh code/stream.js
+++ Fresh code/stream.js
@@ -15,3 +15,9 @@ console.log('END');
 rs.on('error',function(err){
 console.log('ERROR'+err);
 });
+
+var fs = require('fs');
+var rs = fs.createReadStream('sample.txt');
+var ws = fs.createWriteStream('copied.txt');
+rs.pipe(ws);
+
