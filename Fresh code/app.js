2d8f7c2a15d6673d4de7be9566bdfd9668e62a0b
fei zhou <kobefaith@163.com>
2016-01-21 22:11:15 +0800

    add the app.js

--- Fresh code/app.js
+++ Fresh code/app.js
@@ -5,3 +5,16 @@ res.write('<h1>Node.js</h1>');
 res.end('<p>hello world</p>');
 }).listen(3000);
 console.log("Http server is listening at port 3000.");
+
+var fs = require('fs');
+
+fs.readFile('file.txt','utf-8',function(err,data){
+    if(err){
+        console.error(err);
+    }else{
+        console.log(data);
+    }
+}) ;
+console.log('end');
+
+
