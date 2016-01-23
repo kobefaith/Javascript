87a0e4a17e3f83cba3e7dfca10f93e33285cbc4d
fei zhou <kobefaith@163.com>
2016-01-23 23:08:43 +0800

    add the app.js

--- Fresh code/app.js
+++ Fresh code/app.js
@@ -41,6 +41,19 @@ myModule.setName('BYVoid');
 myModule.sayHello();
 
 
+
+function Hello(){
+    var name;
+    this.setName = function(thyName){
+        name = thyName;
+    };
+    this.sayHello = function(){
+        console.log('Hello'+name);
+    };
+    
+};
+module.exports = Hello;
+
     
 
 
