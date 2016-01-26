eb24795dad64f64f1e2743d67c4327c6117a5615
fei zhou <kobefaith@163.com>
2016-01-26 21:31:08 +0800

    add the debug.js

--- Fresh code/debug.js
+++ Fresh code/debug.js
@@ -6,3 +6,7 @@ console.log('hello'+x+a);
 c(b);
 
 console.log(process.argv);
+process.stdin.resume();
+process.stdin.on('data',function(data){
+    process.stdout.write('read from console:'+data.toString());
+})
