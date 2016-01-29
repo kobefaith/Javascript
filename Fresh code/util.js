3f880e8c9c2606aa797a23e24d29c9dfaba5d754
fei zhou <kobefaith@163.com>
2016-01-29 23:01:01 +0800

    add the util.js

--- Fresh code/util.js
+++ Fresh code/util.js
@@ -13,3 +13,19 @@ console.log(this.name);
 function Sub(){
 this.name = 'sub';
 }
+
+var util = require('util');
+
+function Person(){
+    this.name = 'byvoid';
+    this.toString = function(){
+        return this.name;
+    };
+}
+var obj = new Person();
+console.log(util.inspect(obj));
+console.log(util.inspect(obj,true));
+
+
+
+
