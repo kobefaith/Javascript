4b9f90e7946caaa80d7cb6fcb9b9aac65044c89b
fei zhou <kobefaith@163.com>
2016-02-03 22:22:16 +0800

    add the httpserver.js

--- Fresh code/httpserver.js
+++ Fresh code/httpserver.js
@@ -16,3 +16,12 @@ server.on('request',function(req,res){
 });
 server.listen(3000);
 console.log("Http server is listening at port 3000.");
+
+
+var http = require('http');
+var url = require('url');
+vat util = require('util');
+http.createServer(function(req,res){
+    res.writeHead(200,{'Content-Type':'text/plain'});
+    res.end(util.inspect(url.parse(req.url,true)));
+}).listen(3000);
