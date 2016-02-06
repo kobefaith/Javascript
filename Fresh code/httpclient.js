97b51b067b1112ec2ac08927ade72e8ae764d4ce
fei zhou <kobefaith@163.com>
2016-02-06 21:27:59 +0800

    add the client.js

--- Fresh code/httpclient.js
+++ Fresh code/httpclient.js
@@ -23,3 +23,12 @@ console.log(data);
 });
 req.write(contents);
 req.end();
+
+
+var http = require('http');
+http.get({host:'www.byvoid.com'},function(res){
+    res.setEncoding('utf8');
+    res.on('data',function(data){
+        console.log(data);
+    });
+});
