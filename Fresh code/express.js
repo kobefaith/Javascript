dffea04036a934602fedef37c70b43f1423e4b7f
fei zhou <kobefaith@163.com>
2016-02-10 22:09:05 +0800

    add the express.js

--- Fresh code/express.js
+++ Fresh code/express.js
@@ -9,3 +9,27 @@ app.listen(3000);
 app.get('/user/:username',function(req,res){
     res.send('user:' + req.params.username);
 });
+
+
+var express = require('express');
+,routes = require('./routes');
+var app = modules.exports = express.createServer();
+app.configure(function(){
+    app.set('views',__dirname + '/views');
+    app.set('view engine','ejs');
+    app.use(express.bodyParser());
+    app.use(express.methodOverride());
+    app.use(app.route);
+    app.use(express.static(__dirname + '/public'));
+});
+app.configure('development',function(){
+    app.use(express.errorHandler({dumpExceptions:true,showStack:true}));
+});
+
+app.configure('production',function(){
+    app.use(express.errorHandler());
+});
+
+
+app.get('/',routes.index);
+app.listen(3000);
