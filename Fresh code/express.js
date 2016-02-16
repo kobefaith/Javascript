1be0fb2743cd910b9ddd38338db63d850c22e4b6
fei zhou <kobefaith@163.com>
2016-02-16 23:28:18 +0800

    add the express.js

--- Fresh code/express.js
+++ Fresh code/express.js
@@ -65,3 +65,17 @@ app.use(express.session({
     })
 }));
 app.use(express.route(routes));
+
+
+module.exports = function(app){
+    app.get('/',function(req,res){
+        res.render('index',{
+            title:'首页'
+        });
+    });
+    app.get('/reg',function(req,res){
+        res.render('reg',{
+            title:'用户注册',
+        });
+    });
+};
