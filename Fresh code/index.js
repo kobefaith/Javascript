f9c613b6c3dbcf6e51747aeb89d86611a7bfc32a
fei zhou <kobefaith@163.com>
2016-02-19 22:57:10 +0800

    add the index.js

--- Fresh code/index.js
+++ Fresh code/index.js
@@ -3,3 +3,25 @@ res.render('login',{
 title:'用户登录',
 });
 });
+app.post('/login',function(req,res){
+    var md5 = crypto.createHash('md5');
+    var password = md5.update(req.body.password).digest('base64');
+    User.get(req.body.username,function(err,user){
+        if(!user){
+            req.flash('error','用户不存在');
+            return res.redirect('/login');
+        }
+        if(user.password != password){
+            req.flash('error','用户口令错误');
+            return res.redirect('/login');
+        }
+        req.session.user = user;
+        req.flash('sucess','登录成功');
+        res.redirect('/');
+    });
+});
+app.get('/logout',function(req,res){
+   req.session.user = null;
+   req.flash('sucess','登出成功') ;
+   res.redirect('/');   
+});
