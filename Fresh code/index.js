850113f2f1b0c219bb8fe9ab54b26a6896796157
fei zhou <kobefaith@163.com>
2016-02-23 22:56:32 +0800

    add the index.js

--- Fresh code/index.js
+++ Fresh code/index.js
@@ -85,5 +85,17 @@ function checkNotLogin(req,res,next){
     }
     next();
 }
-
+app.post('/post',checkLogin);
+app.post('/post',function(req,res){
+    var currentUser = req.session.user;
+    var post = new Post(currentUser.name,req.body.post);
+    post.save(function(err){
+       if(err){
+           req.flash('error',err);
+           return res.redirect('/');
+       }
+       req.flash('sucess','发表成功');
+       res.redirect('/u/' + currentUser.name);
+    });
+});
 
