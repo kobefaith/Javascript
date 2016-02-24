6523868bcf554e515892e8cd6758f6bb904e5cc5
fei zhou <kobefaith@163.com>
2016-02-24 22:50:34 +0800

    add the index.js

--- Fresh code/index.js
+++ Fresh code/index.js
@@ -98,4 +98,22 @@ app.post('/post',function(req,res){
        res.redirect('/u/' + currentUser.name);
     });
 });
+app.get('/u/:user',function(req,res){
+    User.get(req.params.user,function(err,user){
+        if(!user){
+            req.flash('error','用户不存在');
+            return res.redirect('/');
+        }
+        Post.get(user.name,function(err,posts){
+            if(err){
+                req.flash('error',err);
+                return res.redirect('/');
+            }
+            res.render('user',{
+                title:user.name,
+                posts:posts,
+            });
+        });
+    });   
+});
 
