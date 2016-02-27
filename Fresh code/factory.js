856cddfa1972f64228b62e5810d8bb98c7ccfb14
fei zhou <kobefaith@163.com>
2016-02-27 23:25:11 +0800

    add the factory.js

--- Fresh code/factory.js
+++ Fresh code/factory.js
@@ -1,3 +1,12 @@
 function createPerson(name,age,job){
-
+    var o = new Object();
+    o.name = name;
+    o.age = age;
+    o.job = job;
+    o.sayName = function(){
+        alert(this.name);
+    };
+    return o;
 }
+var person1 = createPerson("Nocholas",29,"software engineer");
+var person2 = createPerson("Greg",27,"Doctor");
