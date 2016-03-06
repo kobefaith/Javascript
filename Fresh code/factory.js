18f673365ba163e72eaaf362a2c68a85bef9654e
fei zhou <kobefaith@163.com>
2016-03-06 22:32:51 +0800

    add the factory.js

--- Fresh code/factory.js
+++ Fresh code/factory.js
@@ -59,3 +59,23 @@ server{
     }
 }
 
+var someuser = {
+    name : 'byvoid',
+    func:function(){
+        console.log(this.name);
+    }
+};
+
+var foo = {
+    name:'foobar'
+};
+foo.func = someuser.func;
+foo.func();
+
+foo.func1 = someuser.func.bind(someuser);
+foo.func1();
+func = someuser.func.bind(foo);
+func();
+func2 = func;
+func2();
+
