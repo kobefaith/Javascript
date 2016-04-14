3fe5349fcdc10f2dd79b1e6447bf2b28d88e6b4d
fei zhou <kobefaith@163.com>
2016-04-14 23:18:36 +0800

    add the change

--- Fresh code/suanfa.cpp
+++ Fresh code/suanfa.cpp
@@ -72,7 +72,19 @@ ListNode *reverse(ListNode *head)
     return pre;
 }
 
-
+ListNode *findmiddle(ListNode *head)
+{
+    if(head == NULL){
+        return NULL;
+    }
+    ListNode *slow = head;
+    ListNode *fast = head->next;
+    while(fast != NULL && fast->next != NULL){
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+    return slow;
+}
 
 
 
