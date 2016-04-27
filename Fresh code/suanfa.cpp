accc2bee59b648158e9a273e5f6f3d8674a0fedf
fei zhou <kobefaith@163.com>
2016-04-27 23:16:44 +0800

    add the change

--- Fresh code/suanfa.cpp
+++ Fresh code/suanfa.cpp
@@ -359,7 +359,19 @@ int main()
     }
 }
 
+#include<stdio.h>
+#include<sys.types.h>
+#include<unistd.h>
 
+int main(void)
+{
+    int i;
+    for(i = 0; i < 2; i++){
+        fork();
+        printf("_");        
+    }
+    return 0;
+}
 
 
 
