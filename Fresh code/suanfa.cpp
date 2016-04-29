33a928b5b7c5ba8f5242f63a53aef5a37563e233
fei zhou <kobefaith@163.com>
2016-04-29 22:55:48 +0800

    add the suanfa

--- Fresh code/suanfa.cpp
+++ Fresh code/suanfa.cpp
@@ -440,5 +440,42 @@ void child(int len, char ch)
      }
  }
 
+#include<iostream>
+using namespace std;
+#include<sys/ipc.h>
+#include<sys/msg.h>
+#include<stdlib.h>
+
+int main(int argc, char* argv[])
+{
+    if(argc == 1){
+        cout<<*argv<<"key"<<endl;
+        return 0;
+    }
+    int key = atoi(argv[1]);
+    int qid;
+    qid = msgget(key,IPC_CREAT|0600);
+    if(qid<0){
+        cout<<"cannot creat"<<endl;
+        return -1;
+    }
+    cout<<"OK!"<<endl;
+    cout<<"key = 0X"<<hex<<key<<endl;
+    cout<<"qid="<<dec<<qid<<endl;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
