c0bda5d4a19de38fed84cb6a55796c9eab775e18
fei zhou <kobefaith@163.com>
2016-05-06 23:14:36 +0800

    add the jquery note

---
+++ Jquery_note.js
@@ -0,0 +1,10 @@
+<head lang="en">
+<meta charset="UTF-8">
+<script src="jquery-2.1.1.min.js"></script>
+<script src="app.js"></script>
+</head>
+
+app.js中
+$(document).ready(function(){
+    alert("文档加载完毕！");
+});
