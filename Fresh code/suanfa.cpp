281cf79b8a83ca5359fe3754f9bc7f059755dbc0
fei zhou <kobefaith@163.com>
2016-06-20 22:04:55 +0800

    add the navigator

--- Fresh code/suanfa.cpp
+++ Fresh code/suanfa.cpp
@@ -610,7 +610,15 @@ var second = d.getSeconds();
 
 </script>
 
-
+function hasPlugin(name){
+    name = name.toLowerCase();
+    for(var i = 0; i < navigator.plugins.length;i++){
+        if(navigator.plugins[i].name.toLowerCase().indexOf(name) > -1){
+            return true;
+        }
+    }
+    return false;
+}
 
 
 
