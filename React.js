a05b9a4c022298637d5d520e9ec6cff490c75108
fei zhou <kobefaith@163.com>
2016-07-10 18:29:32 +0800

    add the react

--- React.js
+++ React.js
@@ -141,14 +141,172 @@ ReactDOM.render(
 组件并不是真实的 DOM 节点，而是存在于内存之中的一种数据结构，叫做虚拟 DOM （virtual DOM）。
 只有当它插入文档以后，才会变成真实的 DOM 。根据 React 的设计，所有的 DOM 变动，都先在虚拟 DOM 上发生，
 然后再将实际发生变动的部分，反映在真实 DOM上，这种算法叫做 DOM diff ，它可以极大提高网页的性能表现。
+但是，有时需要从组件获取真实 DOM 的节点，这时就要用到 ref 属性
+var MyComponent = React.createClass({
+  handleClick: function() {
+    this.refs.myTextInput.focus();
+  },
+  render: function() {
+    return (
+      <div>
+        <input type="text" ref="myTextInput" />
+        <input type="button" value="Focus the text input" onClick={this.handleClick} />
+      </div>
+    );
+  }
+});
 
+ReactDOM.render(
+  <MyComponent />,
+  document.getElementById('example')
+);
 
+上面代码中，组件 MyComponent 的子节点有一个文本输入框，用于获取用户的输入。
+这时就必须获取真实的 DOM 节点，虚拟 DOM 是拿不到用户输入的。
+为了做到这一点，文本输入框必须有一个 ref 属性，然后 this.refs.[refName] 就会返回这个真实的 DOM 节点。
+React 组件支持很多事件，除了 Click 事件以外，还有 KeyDown 、Copy、Scroll 等
+组件免不了要与用户互动，React 的一大创新，就是将组件看成是一个状态机，一开始有一个初始状态，然后用户互动，导致状态变化，从而触发重新渲染 UI 
+var LikeButton = React.createClass({
+  getInitialState: function() {
+    return {liked: false};
+  },
+  handleClick: function(event) {
+    this.setState({liked: !this.state.liked});
+  },
+  render: function() {
+    var text = this.state.liked ? 'like' : 'haven\'t liked';
+    return (
+      <p onClick={this.handleClick}>
+        You {text} this. Click to toggle.
+      </p>
+    );
+  }
+});
 
+ReactDOM.render(
+  <LikeButton />,
+  document.getElementById('example')
+);
 
+上面代码是一个 LikeButton 组件，它的 getInitialState 方法用于定义初始状态，
+也就是一个对象，这个对象可以通过 this.state 属性读取。
+当用户点击组件，导致状态变化，this.setState 方法就修改状态值，
+每次修改以后，自动调用 this.render 方法，再次渲染组件。
+由于 this.props 和 this.state 都用于描述组件的特性，可能会产生混淆。
+一个简单的区分方法是，this.props 表示那些一旦定义，就不再改变的特性，
+而 this.state 是会随着用户互动而产生变化的特性。
 
+用户在表单填入的内容，属于用户跟组件的互动，所以不能用 this.props 读取
+var Input = React.createClass({
+  getInitialState: function() {
+    return {value: 'Hello!'};
+  },
+  handleChange: function(event) {
+    this.setState({value: event.target.value});
+  },
+  render: function () {
+    var value = this.state.value;
+    return (
+      <div>
+        <input type="text" value={value} onChange={this.handleChange} />
+        <p>{value}</p>
+      </div>
+    );
+  }
+});
 
+ReactDOM.render(<Input/>, document.body);
 
+组件的生命周期分成三个状态：
+Mounting：已插入真实 DOM
+Updating：正在被重新渲染
+Unmounting：已移出真实 DOM
 
+React 为每个状态都提供了两种处理函数，will 函数在进入状态之前调用，did 函数在进入状态之后调用，三种状态共计五种处理函数。
+componentWillMount()
+componentDidMount()
+componentWillUpdate(object nextProps, object nextState)
+componentDidUpdate(object prevProps, object prevState)
+componentWillUnmount()
+
+两种特殊状态处理
+componentWillReceiveProps(object nextProps)：已加载组件收到新的参数时调用
+shouldComponentUpdate(object nextProps, object nextState)：组件判断是否重新渲染时调用
+
+var Hello = React.createClass({
+  getInitialState: function () {
+    return {
+      opacity: 1.0
+    };
+  },
+
+  componentDidMount: function () {
+    this.timer = setInterval(function () {
+      var opacity = this.state.opacity;
+      opacity -= .05;
+      if (opacity < 0.1) {
+        opacity = 1.0;
+      }
+      this.setState({
+        opacity: opacity
+      });
+    }.bind(this), 100);
+  },
+
+  render: function () {
+    return (
+      <div style={{opacity: this.state.opacity}}>
+        Hello {this.props.name}
+      </div>
+    );
+  }
+});
+
+ReactDOM.render(
+  <Hello name="world"/>,
+  document.body
+);
+上面代码在hello组件加载以后，通过 componentDidMount 方法设置一个定时器，每隔100毫秒，就重新设置组件的透明度，从而引发重新渲染。
+另外，组件的style属性的设置方式也值得注意，不能写成
+style="opacity:{this.state.opacity};"
+而要写成
+style={{opacity: this.state.opacity}}
+
+Ajax
+var UserGist = React.createClass({
+  getInitialState: function() {
+    return {
+      username: '',
+      lastGistUrl: ''
+    };
+  },
+
+  componentDidMount: function() {
+    $.get(this.props.source, function(result) {
+      var lastGist = result[0];
+      if (this.isMounted()) {
+        this.setState({
+          username: lastGist.owner.login,
+          lastGistUrl: lastGist.html_url
+        });
+      }
+    }.bind(this));
+  },
+
+  render: function() {
+    return (
+      <div>
+        {this.state.username}'s last gist is
+        <a href={this.state.lastGistUrl}>here</a>.
+      </div>
+    );
+  }
+});
+
+ReactDOM.render(
+  <UserGist source="https://api.github.com/users/octocat/gists" />,
+  document.body
+);
 
 
 
