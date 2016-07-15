a6f40124f34a417cba930c836b357394154575b4
fei zhou <kobefaith@163.com>
2016-07-15 23:39:11 +0800

    add

--- JS_note.js
+++ JS_note.js
@@ -1291,7 +1291,10 @@ for (var i = 0; i < 5; ++i) {
     console.log(i + ' ');
   }, 100);
 }    
- 不能输出正确结果，因为循环中setTimeout接受的参数函数通过闭包访问变量i。javascript运行环境为单线程，setTimeout注册的函数需要等待线程空闲才能执行，此时for循环已经结束，i值为5.五个定时输出都是5 修改方法：将setTimeout放在函数立即调用表达式中，将i值作为参数传递给包裹函数，创建新闭包
+ 不能输出正确结果，因为循环中setTimeout接受的参数函数通过闭包访问变量i。
+ javascript运行环境为单线程，setTimeout注册的函数需要等待线程空闲才能执行，
+ 此时for循环已经结束，i值为5.五个定时输出都是5 修改方法：将setTimeout放在函数立即调用表达式中，
+ 将i值作为参数传递给包裹函数，创建新闭包
 
 for (var i = 0; i < 5; ++i) {
   (function (i) {
