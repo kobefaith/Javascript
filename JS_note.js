12e3158a33cb9900b9c7f3b070dc872f7f31a568
fei zhou <kobefaith@163.com>
2016-08-04 23:06:10 +0800

    add

--- JS_note.js
+++ JS_note.js
@@ -1363,7 +1363,63 @@ the x and y coordinates of the mouse pointer.</p>
 
 </body>
 </html>   
+H5实现拖拽，在两个边框中拖拽图片。
+首先，为了使元素可拖动，把 draggable 属性设置为 true ：<img draggable="true" />
+拖动什么 - ondragstart 和 setData()
+
+然后，规定当元素被拖动时，会发生什么。
+在上面的例子中，ondragstart 属性调用了一个函数，drag(event)，它规定了被拖动的数据。
+dataTransfer.setData() 方法设置被拖数据的数据类型和值：
+function drag(ev){
+   ev.dataTransfer.setData("Text",ev.target.id);
+}
+放到何处 - ondragover
+ondragover 事件规定在何处放置被拖动的数据。
+默认地，无法将数据/元素放置到其他元素中。如果需要设置允许放置，我们必须阻止对元素的默认处理方式。
+这要通过调用 ondragover 事件的 event.preventDefault() 方法：
+进行放置 - ondrop
+当放置被拖数据时，会发生 drop 事件。
+在上面的例子中，ondrop 属性调用了一个函数，drop(event)：
+function drop(ev)
+{
+    ev.preventDefault();
+    var data=ev.dataTransfer.getData("Text");
+    ev.target.appendChild(document.getElementById(data));
+}
+代码解释：
+
+    调用 preventDefault() 来避免浏览器对数据的默认处理（drop 事件的默认行为是以链接形式打开）
+    通过 dataTransfer.getData("Text") 方法获得被拖的数据。该方法将返回在 setData() 方法中设置为相同类型的任何数据。
+    被拖数据是被拖元素的 id ("drag1")
+    把被拖元素追加到放置元素（目标元素）中
+===========================
+<script type="text/javascript">
+function allowDrop(ev)
+{
+    ev.preventDefault();//阻止默认事件
+}
+
+function drag(ev)
+{
+   ev.dataTransfer.setData("Text",ev.target.id);
+}
+
+function drop(ev)
+{
+   ev.preventDefault();
+   var data=ev.dataTransfer.getData("Text");
+   ev.target.appendChild(document.getElementById(data));
+}
+</script>
+</head>
+<body>
+
+<div id="div1" ondrop="drop(event)" ondragover="allowDrop(event)">
+  <img src="/i/w3school_logo_black.gif" draggable="true" ondragstart="drag(event)" id="drag1" />
+</div>
+<div id="div2" ondrop="drop(event)" ondragover="allowDrop(event)"></div>
     
+      
     
     
     
