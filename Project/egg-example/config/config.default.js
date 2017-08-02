bf3283be01f3912da7dee20d5f994e37ea7435d1
kobe <kobe@promote.cache-dns.local>
2017-08-02 22:10:45 +0800

    add the egg

---
+++ Project/egg-example/config/config.default.js
@@ -0,0 +1,12 @@
+'use strict';
+
+module.exports = appInfo => {
+  const config = {};
+
+  // should change to your own
+  config.keys = appInfo.name + '_1501681473256_8368';
+
+  // add your config here
+
+  return config;
+};
