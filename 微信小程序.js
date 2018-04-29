e6d3132447609ae153d90efa42348cab61df41b1
kobe <kobefaith@163.com>
2018-04-29 23:23:34 +0800

    add fenbao

--- 微信小程序.js
+++ 微信小程序.js
@@ -8,3 +8,4 @@ wepy build --watch
 关闭ES6转ES5。重要：漏掉此项会运行报错。
 关闭上传代码时样式自动补全 重要：某些情况下漏掉此项会也会运行报错。
 项目-->关闭代码压缩上传 重要：开启后，会导致真机computed, props.sync 等等属性失效。#270
+小程序分包加载。https://blog.csdn.net/acmdown/article/details/80037660
